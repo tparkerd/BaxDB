78d42b696037162cfc51d6b94d91392b324fb786
Tim Parker <Tim.ParkerD@gmail.com>
2019-01-08 16:45:44 -0600

    Added source to target files comparison for CSV A transformer

--- lib/transforms/test/conftest.py
+++ lib/transforms/test/conftest.py
@@ -24,3 +24,20 @@ def data_csv():
 def args_csv():
   with patch('sys.argv', ['-v', '--debug', '-t', 'csv', './test/data/csv']):
     return po()
+
+@pytest.fixture(scope='module')
+def data_csv_a():
+  try:
+    # Float precision helps to avoid rounding errors, but it does hurt
+    # performance
+    df = pd.read_csv('./test/data/csv_a', float_precision='round_trip',
+                      index_col='Pedigree')
+  except:
+    raise
+
+  return df
+
+@pytest.fixture(scope='module')
+def args_csv_a():
+  with patch('sys.argv', ['-v', '--debug', '-t', 'csv_a', './test/data/csv_a']):
+    return po()
