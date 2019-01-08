a821bed359e456eefb11cfa6e52def45bb5cf867
Tim Parker <Tim.ParkerD@gmail.com>
2019-01-08 15:42:08 -0600

    Removed debug statements

--- lib/transforms/test/test_csv.py
+++ lib/transforms/test/test_csv.py
@@ -2,7 +2,6 @@
 Unit tester module for verifying the output of the `splitLongFormat` module
 """
 import pytest
-from pprint import pprint
 from lib.transformer.csv import process
 from lib.helpers import Convert
 import math
@@ -39,27 +38,21 @@ def test_csv(args_csv, data_csv):
       # line should *not* be in the target file if there was no growout for said
       # line. However, if the line was part of the growout, but that value was
       # not measured, the value will be there, and it has to be checked for NAN
-      print(f'{index_name}   <{target_name}>   ({src_col_name} -> {target_col_name})   sv: {src_value}', end = '')
       if math.isnan(src_value):
         # Check that index is not in target dataframe
         if index_name in target_data.index:
           # Make sure that is is also NAN, otherwise we have an error
           target_value = target_data.loc[[index_name], [target_col_name]].values[0,0]
-          print(f'   tv: {target_value}')
           src_processed_count = src_processed_count + 1 # Got to a valid comparison, so we can consider it processed
           assert math.isnan(target_value), f'Source value {target_value} is not considered NAN.'
         # Source NAN value was omitted from target file
         # Line was not present in growout (aka, location & year)
         else:
-          print(f'   [{index_name} not in {target_name}]')
+          pass
       else:
-        target_value = target_data.loc[[index_name], [target_col_name]].values[0,0]
-        print(f'   tv: {target_value}')
         src_processed_count = src_processed_count + 1 # Got to a valid comparison, so we can consider it processed
+        target_value = target_data.loc[[index_name], [target_col_name]].values[0,0]
         assert math.isclose(src_value, target_value, rel_tol=1e-20), f'Values {src_value}, {target_value} are not close enough to be considered equal.'
-
-
-  print("\n\n===================================================================== Test 1.1 Complete =====================================================================\n")
 
   # Compare each value from targets to source
   # For each target file...
@@ -80,7 +73,6 @@ def test_csv(args_csv, data_csv):
         # combining with the filename. Use it to access the source dataset
         src_col_name = Convert.column_to_trait(target_col_name, target_name)
         src_value = src_data.loc[[index_name], [src_col_name]].values[0,0]
-        print(f'{index_name}   <{target_name}>   ( {target_col_name} -> {src_col_name} )   tv: {target_value}    sv: {src_value}')
 
         target_processed_count = target_processed_count + 1 # Got to a valid comparison, so we can consider it processed
         # Check for NANs, otherwise make sure the values are relatively equal
@@ -89,9 +81,6 @@ def test_csv(args_csv, data_csv):
         else:
           assert math.isclose(target_value, src_value, rel_tol=1e-20), f'Values {target_value}, {src_value} are not close enough to be considered equal.'
         
-
-  print("\n\n===================================================================== Test 1.2 Complete =====================================================================\n")
-
   # Successfully processed from source > targets *and* targets > source
   # Check that the same number of values were compared
   difference = src_processed_count - target_processed_count
